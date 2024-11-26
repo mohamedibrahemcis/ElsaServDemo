2c1eb76a93a9d9025c7480ca7f55b0d2fea7612c
mohamedibrahemcis <mohamedibrahemcis@gmail.com>
2024-11-26 13:55:27 +0300

    init elsa

--- src/installation/elsa-server-and-studio/ElsaServer/Program.cs
+++ src/installation/elsa-server-and-studio/ElsaServer/Program.cs
@@ -52,4 +52,33 @@ app.UseWorkflowsApi();
 app.UseWorkflows();
 app.UseWorkflowsSignalRHubs();
 app.MapFallbackToPage("/_Host");
+
+// Create a sample weather forecast API.
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+        .ToArray();
+    return forecast;
+});
+
+
+
+
+
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
